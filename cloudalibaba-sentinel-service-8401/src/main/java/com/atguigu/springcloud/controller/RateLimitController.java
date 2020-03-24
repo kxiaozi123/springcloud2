fdc5db6d2dbef3998d80ff3ddb09d2bdd9fc1d91
kxiaozi123 <511158469@qq.com>
2020-03-24 15:48:46 +0800

    sentinel入门级讲解完毕

---
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -0,0 +1,52 @@
+package com.atguigu.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.myhandler.CustomerBlockHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 15:04
+ */
+@RestController
+public class RateLimitController
+{
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource()
+    {
+        //关闭服务后，流控规则消失了 所以是临时的
+        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
+    }
+    public CommonResult handleException(BlockException exception)
+    {
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+    //        上面兜底方案面临的问题
+//        1系统默认的，没有体现我们自己的业务要求。
+//        2依照现有条件，我们自定义的处理方法又和业务代码耦合在-块，不直观。
+//        3每个业务方法都添加一个兜底的，那代码膨胀加剧。
+//        4 全局统--的处理方法没有体现。
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl()
+    {
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+
+    }
+
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler()
+    {
+        return new CommonResult(200,"按客戶自定义",new Payment(2020L,"serial003"));
+    }
+}
