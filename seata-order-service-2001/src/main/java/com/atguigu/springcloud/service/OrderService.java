99f3a591159d4311216e347959203f9686e25c28
kxiaozi123 <511158469@qq.com>
2020-03-25 14:04:50 +0800

    seata部分完成

---
+++ seata-order-service-2001/src/main/java/com/atguigu/springcloud/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.service;
+
+
+import com.atguigu.springcloud.domain.Order;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:19
+ */
+public interface OrderService
+{
+    void create(Order order);
+}
